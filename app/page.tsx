5d3b9b6670efd47a5dd9a5b0b78c99d3657994bd
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-09 20:06:10 -0500

    seteo

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,51 @@
 "use client"
 import RandomFox from '../components/RandomFox'
+import type {MouseEventHandler} from 'react'
 import {useState} from 'react'
-
-const random = () => {
-      return Math.floor(Math.random() * 123 ) + 1
-  }
 
 type ImageItems = {
   id: string, 
   url: string
 }
+
+const random = () => {
+      return Math.floor(Math.random() * 123 ) + 1
+  }
+
 const generateID = () => Math.random().toString(36).substr(2,9)
+
 export default function Home() {
 
   // los genericcos es definir el useState
   // esto significa que va a aser un array de tipo string solo string el usetate<string>([])
-  // const [image, setimage] = useState<string[]>([
+  // const [image, setImage] = useState<string[]>([
   //   `https://randomfox.ca/images/${random()}.jpg`,
   //   `https://randomfox.ca/images/${random()}.jpg`,
   //   `https://randomfox.ca/images/${random()}.jpg`,
   //   `https://randomfox.ca/images/${random()}.jpg`,
   // ])
 
-  const [image, setimage] = useState<Array<ImageItems>>([
-    {id: generateID(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateID(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateID(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateID(), url:`https://randomfox.ca/images/${random()}.jpg`},
-    
-  ])
+  const [image, setImage] = useState<Array<ImageItems>>([])
 
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
 
+    const target = event.target
+    const newImageItem: ImageItems = {
+      id: generateID(),
+      url: `https://randomfox.ca/images/${random()}.jpg`
+    }
+    setImage([
+      ...image,
+      newImageItem
+    ]
+    )
+  }
 
   return (
     <>
       <h1 className='caret-violet-600'>Hello</h1>
-
+      <button onClick={addNewFox}>Add new Fox</button>
       {image.map(({id, url}) => ( 
         <div key={id} className='p-4'>
           <RandomFox image={url}/>
--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,4 +1,4 @@
-
+import {useRef} from 'react'
 // LOS PROPS SON UN MODELO EN EL CUAL SE MANDARA EL VALOR
 type Props = {
     image: string
@@ -6,9 +6,11 @@ type Props = {
 
 //SIEMPRE SE LLAMA LOS VALORES DE LOS PROPS Y EL NOMBRE DEL MODELO
 const RandomFox = ({image}:Props):JSX.Element => {
+    const node = useRef<HTMLImageElement>(null)
+
     return (
         <>
-            <img src={image} className="h-64 w-auto rounded-3xl"></img>
+            <img ref={node} src={image} className="h-64 w-auto rounded-3xl"></img>
         </>
     )
 }
