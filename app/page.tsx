7737f05b63d7a1fc6ae7e116027bdecf7862f9e8
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-15 21:07:00 -0500

    update project

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,57 @@
 "use client"
 import LazyImage from '../components/LazyImage'
-import type {MouseEventHandler} from 'react'
-import {useState} from 'react'
+import type { MouseEventHandler } from 'react'
+import { useState } from 'react'
 import { random } from 'lodash'
 
-const myRandom = () => {
-    random(1, 123)
-  }
+// No necesitas importar 'url' e 'inspector' ya que no los estás usando
 
-const generateID = () => Math.random().toString(36).substr(2,9)
+interface IFoxItem {
+  id: string;
+  url: string;
+}
+
+const generateID = () => Math.random().toString(36).substr(2, 9)
 
 export default function Home() {
-
-  // los genericcos es definir el useState
-  // esto significa que va a aser un array de tipo string solo string el usetate<string>([])
-  // const [image, setImage] = useState<string[]>([
-  //   `https://randomfox.ca/images/${random()}.jpg`,
-  //   `https://randomfox.ca/images/${random()}.jpg`,
-  //   `https://randomfox.ca/images/${random()}.jpg`,
-  //   `https://randomfox.ca/images/${random()}.jpg`,
-  // ])
-
   const [image, setImage] = useState<Array<IFoxItem>>([])
 
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.preventDefault()
 
-    const target = event.target
     const newImageItem: IFoxItem = {
       id: generateID(),
-      url: `https://randomfox.ca/images/${myRandom()}.jpg`
-    }
-    setImage([
-      ...image,
-      newImageItem
-    ]
-    )
+      url: `https://randomfox.ca/images/${random(1, 123)}.jpg`
+    };
+
+    setImage([...image, newImageItem]);
+    window.plausible("add_fox");
   }
 
   return (
-    <>
-      <h1 className='caret-violet-600'>Hello</h1>
-      <button onClick={addNewFox}>Add new Fox</button>
-      <main>
-        <div className="m-4">
-          <button
-            onClick={addNewFox}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          >
-            Add new fox
-          </button>
-        </div>
+    <div className="container mx-auto mt-8">
+      <h1 className='text-4xl font-bold mb-4'>Hello</h1>
+      <button
+        onClick={addNewFox}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Add new Fox
+      </button>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {image.map(({ id, url }, index) => (
-          <div className="p-4" key={id}>
+          <div key={id}>
             <LazyImage
               src={url}
               width="320"
               height="auto"
-              className="mx-auto rounded-md bg-gray-300"
-              onClick={() => {
-                console.log("holi!");
-              }}
-              onLazyLoad={(img) => {
-                console.log(`Image #${index + 1} cargada. Nodo:`, img);
-              }}
+              className="mx-auto rounded-md bg-gray-300 cursor-pointer"
+              onClick={() => console.log("holi!")}
+              onLazyLoad={(img) => console.log(`Image #${index + 1} loaded. Node:`, img)}
             />
           </div>
         ))}
-      </main>
-    </>
+      </div>
+    </div>
   )
 }
